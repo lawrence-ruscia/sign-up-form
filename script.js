62b7553f88433601a8d3048ccb426b6201ee6245
Renz <140231442+lawrnzzz@users.noreply.github.com.>
2024-10-11 15:54:57 +0800

    style: add red borders to input fields that is invalid but have content, this is to prevent too much noise when the user has just entered the field

---
+++ script.js
@@ -0,0 +1,6 @@
+/*
+ * 1. Show error messages immediately if issues are severe
+ * 2. Late validation is almost always better
+ * 3. Validate empty fields only on submit
+ * 4. Reward early, punish late
+ */
